f13b66402ccf6e6c0d0c6e884a769b41273e8bd3
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2023-07-26 14:36:44 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,12 +4,12 @@
 // let score = undefined;
 // let score = "Aman";
 let score = true;
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -18,7 +18,7 @@ console.log(valueInNumber);
 
 let loggedIn = "";
 let booleanLoggedIn = Boolean(loggedIn);
-console.log(booleanLoggedIn);
+// console.log(booleanLoggedIn);
 
 //true => 1//false => 0
 //"" => false
@@ -26,7 +26,44 @@ console.log(booleanLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 //2.
+//********************************Operations********************************
+let value = 3;
+let negValue = -value ;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+let str1 = "Hello";
+let str2 = " Aman";
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+//console.log((3+4)*5%3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+); //it will give error
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
