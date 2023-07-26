c6d1c75d402e426fbe3edea5f0b10a8347b435b7
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2023-07-26 14:42:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const id=123;
+let user="Aman";
+var password="mountain";
+aman="social_animal";
+
+//id=456;//not allowed b/c const contain fix value
+user="kumar";
+password="sky";
+aman="amazing";
+
+console.table([id,user,password,aman]);
