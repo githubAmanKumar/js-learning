dbcf80faf4d3686982fcb53248c2926afb5f2ce7
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2023-08-06 16:12:24 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,28 @@
+const name="Aman";
+const repoCount=50;
+// console.log(name + repoCount + " Value");
+// console.log(`Hello my name is ${name} and i have ${repoCount} number of repos on github`);
+
+const gameName = new String('Aman-k-com');
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("n"));
+
+const newString = gameName.substring(0,5);
+// console.log(newString);
+const anotherString = gameName.slice(-1,2);
+// console.log(anotherString);
+const newStringOne = "    Aman    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// const url = "https://"AmanKumar.com/ab%12Site";
+// console.log(url.replace('%12','-'));
+// console.log(url.includes('AmanKumar'));
+console.log(gameName.split('-'));
+
+
