93de91d006d57936faef2a359ac666552d21d016
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2023-09-03 09:15:14 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,6 +2,38 @@
 // Object.create 
 
 //object literals
+
+const mySym = Symbol("key1")
 const jsUser = {
-    name: "Aman"
+    name: "Aman",
+    [mySym]: "mykey1",
+    age: "21",
+    location: "Saharanpur",
+    "full email": "mail@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["mon","sat"]
 }
+
+// console.log(jsUser);
+// console.log(jsUser.name);
+// console.log(jsUser["name"]);
+
+// console.log(jsUser.full email); //will throw error
+// console.log(jsUser["full email"]);
+// console.log(jsUser[mySym]);
+
+jsUser.name = "kumar";
+// console.log(jsUser);
+
+// Object.freeze(jsUser);
+jsUser.name = "kk";
+// console.log(jsUser);
+
+jsUser.greeting = function() {
+    console.log("hello js user");
+}
+jsUser.greetingTwo = function() {
+    console.log(`hello js user, ${this.name}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
