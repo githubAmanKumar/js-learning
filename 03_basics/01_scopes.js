a15426e72d19582611f7d2e32006699f6dc3d36e
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2024-01-07 17:21:22 +0000

    scopeHoist

--- 03_basics/01_scopes.js
+++ 03_basics/01_scopes.js
@@ -4,8 +4,45 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log(a);
+    // console.log(a);
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+//next
+
+function one(){
+    const username = "Aman"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    
+    // console.log(website)
+    two()
+}
+// one()
+
+if(true){
+    const user = "Aman"
+    if(user==="Aman"){
+        const web = " youtube"
+        // console.log(user + web)
+    }
+    // console.log(web)
+}
+// console.log(user)
+
+
+
+//++++++++++Interesting+++++++++++++++++hoist
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+addTwo(5)
+const addTwo= function(num){
+    return num + 2
+}
