82a32a01a86aa7a78a23876b8f69fc9e16bd5b01
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2024-02-24 11:59:04 +0530

    Add files via upload

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,25 @@
+// while
+
+let index=0;
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2;
+}
+
+var myArray = ["Hanuman","Batman","Superman"];
+var i = 0;
+while (i < myArray.length) {
+    // console.log(myArray[i]);
+    i = i + 1;
+}
+
+
+
+// do while
+
+let score = 11;
+
+do {
+    console.log(score);
+    score++;
+} while (score<=10);
