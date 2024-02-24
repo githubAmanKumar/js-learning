ed5f487608aa0ec6f9066d11a4592685332385ba
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2024-02-25 05:22:08 +0530

    Update 03_three.js

--- 05_iterations/03_three.js
+++ 05_iterations/03_three.js
@@ -16,3 +16,23 @@ for (const greet of greeting) {
 }
 
 // Maps
+
+const map = new Map();
+map.set('IN',"India");
+map.set('USA',"United States Of America");
+map.set('Fr',"France");
+map.set('IN',"India");
+// console.log(map);
+
+for (const [key,Value] of map) {
+    // console.log(key,"=>",Value);
+}
+
+const myObject = {
+    'Game1' : "nfs", 
+    'Game2' : "Spiderman", 
+}
+
+// for (const [key,Value] of myObject) {
+//     console.log(key,"=>",Value); //myObject is not iterable
+// }
